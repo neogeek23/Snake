2f0d42bba3f9c1a126f8da32b49edabb8f19b73b
Brad Stein <brad.stein@gmail.com>
2018-01-02 17:17:31 -0600

    Add files via upload

--- snake/snake.js
+++ snake/snake.js
@@ -183,7 +183,7 @@ class Snek{
         var score_rate_element = document.getElementsByClassName("point_rate")[0];
 
         score_element.innerHTML = ((this.length - initial_tail_length) * 100).toString();
-        score_rate_element.innerHTML = ((this.length - initial_tail_length) * 100/this.alive).toString();
+        score_rate_element.innerHTML = ((this.length - initial_tail_length) * 100/this.alive).toPrecision(5).toString();
     }
 };
 
